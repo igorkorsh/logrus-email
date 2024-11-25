b4b847b65ef1fc037c8522169a662c6f87353874
Igor Korshunov <igor.korshunov@logrusit.com>
2024-11-25 23:26:49 +0400

    Версия 1.0.2 - Заменил передачу изображений в компоненте Image - Добавил футер на русском языке - Добавил компонент Webview_v2

--- emails/template.tsx
+++ emails/template.tsx
@@ -18,22 +18,25 @@ import {
 	Spacer,
 	Hero,
 	Webview,
+	Webview_v2,
 	Social,
 	Footer,
-	Footer_v2
+	Footer_v2,
+	Footer_v2_ru
 } from '../src/components'
 
 export default function () {
 	return (
-		<Provider options={{ lang: 'en', colorScheme: 'light dark' }}>
+		<Provider options={{ lang: 'ru', colorScheme: 'light' }}>
 			<Html>
 				<Head title='' />
 				<Body color='#f4f7fa' darkClass='dark-bg-gray-100'>
 					<Preview text='' />
 					<Container>
 						<Webview />
-						<Footer />
-						<Footer_v2 />
+						<Footer variant='light' />
+						<Footer_v2 variant='light' />
+						<Footer_v2_ru variant='light' />
 					</Container>
 				</Body>
 			</Html>
--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -8,13 +8,25 @@ export interface SocialItem {
 }
 
 export interface SocialProps {
+	variant?: 'light' | 'dark' | 'both'
 	size: number
 	gap: number
 }
 
-export const Social = ({ size, gap }: SocialProps) => {
+export const Social = ({ variant = 'light', size, gap }: SocialProps) => {
 	const lang = React.useContext(LangContext)
 	const icons: SocialItem[] = data[lang] || data['en']
+
+	const getImageSet = (label: string) => {
+		switch (variant) {
+			case 'light':
+				return `/static/${label.toLowerCase()}.png`
+			case 'dark':
+				return `/static/${label.toLowerCase()}-dark.png`
+			case 'both':
+				return [`/static/${label.toLowerCase()}.png`, `/static/${label.toLowerCase()}-dark.png`]
+		}
+	}
 
 	return (
 		<Table width={icons.length * (size + gap) - gap} className='float-left'>
@@ -22,12 +34,7 @@ export const Social = ({ size, gap }: SocialProps) => {
 				{icons.map(({ url, label }, index) => (
 					<td key={index} className={`w-[${size + (index < icons.length - 1 ? gap : 0)}px]`}>
 						<Link href={url}>
-							<Image
-								src={`/static/${label.toLowerCase()}.png`}
-								srcDark={`/static/${label.toLowerCase()}-dark.png`}
-								width={size}
-								height={size}
-							/>
+							<Image srcset={getImageSet(label)} width={size} height={size} />
 						</Link>
 					</td>
 				))}
