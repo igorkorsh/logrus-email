1bccaa43d16b3475dc30f8baca6213205bf7c4a4
Igor Korshunov <igor.korshunov@logrusit.com>
2025-05-19 17:53:40 +0400

    fix: убрал подчеркивание у картинок в oft

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,32 +1,32 @@
-import * as React from 'react'
-import type { BaseProps, EmailComponent } from '../types'
-import { cn } from '../utils/classnames'
+import * as React from "react";
+import type { BaseProps, EmailComponent } from "../types";
+import { cn } from "../utils/classnames";
 
-interface LinkProps extends BaseProps<'a'> {}
+interface LinkProps extends BaseProps<"a"> {}
 
 export const Link: EmailComponent<LinkProps> = ({
 	children,
-	href = '#',
-	target = '_blank',
+	href = "#",
+	target = "_blank",
 	className,
 	...props
 }) => {
-	const isText = typeof children === 'string'
+	const isText = typeof children === "string";
 
 	return (
 		<a
 			{...props}
 			href={href}
 			target={target}
-			className={cn('text-green-300 underline', className)}
+			className={cn("text-green-300", isText ? "underline" : "", className)}
 		>
 			{isText ? (
-				<span className={cn('text-green-300 underline', className)}>
+				<span className={cn("text-green-300 underline", className)}>
 					{children}
 				</span>
 			) : (
 				children
 			)}
 		</a>
-	)
-}
+	);
+};
