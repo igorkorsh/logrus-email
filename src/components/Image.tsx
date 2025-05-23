d65f2fc2d35bd590175418e652eff85dfc603d80
Igor Korshunov <igor.korshunov@logrusit.com>
2025-05-23 13:11:05 +0400

    fix(image): image size in dark mode

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react'
-import type { BaseProps, EmailComponent } from '../types'
-import { cn } from '../utils/classnames'
+import * as React from "react";
+import type { BaseProps, EmailComponent } from "../types";
+import { cn } from "../utils/classnames";
 
-interface ImageProps extends BaseProps<'img'> {
-	src: string
-	darkSrc?: string
-	stretch?: boolean
-	classNames?: Partial<Record<'light' | 'dark', string>>
+interface ImageProps extends BaseProps<"img"> {
+	src: string;
+	darkSrc?: string;
+	stretch?: boolean;
+	classNames?: Partial<Record<"light" | "dark", string>>;
 }
 
 export const Image: EmailComponent<ImageProps> = ({
@@ -26,29 +26,31 @@ export const Image: EmailComponent<ImageProps> = ({
 				width={width}
 				height={height}
 				className={cn(
-					'border-none align-top',
+					"border-none align-top",
 					`max-w-[${width}px]`,
-					stretch && 'h-auto w-full max-w-[600px]',
-					darkSrc && 'dark-hidden',
-					classNames?.light
+					stretch && "h-auto w-full max-w-[600px]",
+					darkSrc && "dark-hidden",
+					classNames?.light,
 				)}
 			/>
 			{darkSrc && (
 				<>
-					<span dangerouslySetInnerHTML={{ __html: '<!--[if !mso]><!-->' }} />
+					<span dangerouslySetInnerHTML={{ __html: "<!--[if !mso]><!-->" }} />
 					<img
 						{...props}
 						src={darkSrc}
 						width={width}
 						height={height}
 						className={cn(
-							'dark-block hidden h-auto w-full max-w-[600px] border-none',
-							classNames?.dark
+							"dark-block hidden border-none",
+							`max-w-[${width}px]`,
+							stretch && "h-auto w-full max-w-[600px]",
+							classNames?.dark,
 						)}
 					/>
-					<span dangerouslySetInnerHTML={{ __html: '<!--<![endif]-->' }} />
+					<span dangerouslySetInnerHTML={{ __html: "<!--<![endif]-->" }} />
 				</>
 			)}
 		</>
-	)
-}
+	);
+};
